9d903dc8e6b3ffb21a23f7e4ded2542251dd628c
Akash Ghosh <akashghosh3360@gmail.com>
2026-05-18 19:55:27 +0530

    added backend

---
+++ backend/app.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, jsonify
+import pickle
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # allow React to connect
+
+# Load model
+model = pickle.load(open("model.pkl", "rb"))
+vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
+
+labels = [
+    "toxic",
+    "severe_toxic",
+    "obscene",
+    "threat",
+    "insult",
+    "identity_hate"
+]
+
+# Prediction function
+def predict_comment(text):
+    vec = vectorizer.transform([text])
+    pred = model.predict(vec)[0]
+
+    result = {}
+    for i, label in enumerate(labels):
+        result[label] = int(pred[i])
+
+    return result
+
+# API route
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.json
+    text = data["text"]
+
+    result = predict_comment(text)
+
+    return jsonify({
+        "comment": text,
+        "analysis": result
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
